784dbf95f2fb4d089fdcb0968020608c43a8ed60
Ignacio-Cane <nachicane7@gmail.com>
2023-04-11 10:11:40 -0300

    11.04

--- src/Components/Main/pedido/pedido.js
+++ src/Components/Main/pedido/pedido.js
@@ -6,8 +6,8 @@ const Pedido = () => {
     <Fragment>
         <section>
             <div className='cabecera-pedido'>
-                <h1>Las Pizzas de Nachito</h1>
-                <p>Algo que siempre me gusto de chico fueron las pizzas, entre amigos, familiares, en cumpleaños diria que para cualquier momento de la vida.. como tal vez una simple cena con alguien.
+                <h1 className='cabecera-titulo'>Las Pizzas de Nachito</h1>
+                <p className='cabecera-parrafo'>Algo que siempre me gusto de chico fueron las pizzas, entre amigos, familiares, en cumpleaños diria que para cualquier momento de la vida.. como tal vez una simple cena con alguien.
                     Estoy orgulloso de este emprendimiento familiar, nos dedicamos a esto 100% te vamos asegurar felicidad plena, no dudes en pasar un buen momento... </p>
                     <a className='cabecera-a' href='https://wa.me/3537668071'>
                     <img className='cabecera-wa' src='../../assents/img/wa.png' alt='wa'/>
@@ -18,4 +18,4 @@ const Pedido = () => {
   )
 }
 
-export default Pedido;
+export default Pedido; 
